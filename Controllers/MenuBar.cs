45a45fb056c76ad11235f2b7f15369a02249d2eb
@KaraboMakoro <st10461176@rcconnect.edu.za>
2025-09-04 09:07:22 +0200

    Add project files.

---
+++ Controllers/MenuBar.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ST10461176_PROG_6212_POE.Controllers
+{
+    public class MenuBarController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Views/Home/Index.cshtml");
+        }
+        public IActionResult LecturerPage()
+        {
+            return View("~/Views/Home/LecturerPage.cshtml");
+        }
+        public IActionResult ProgramCoordinatorPage()
+        {
+            return View("~/Views/Home/ProgramCoordinatorPage.cshtml");
+        }
+        public IActionResult AcademicManagerPage()
+        {
+            return View("~/Views/Home/AcademicManagerPage.cshtml");
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ST10461176_PROG_6212_POE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
